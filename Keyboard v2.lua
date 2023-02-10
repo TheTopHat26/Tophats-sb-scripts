af9f2a35dc52f0cc80ac80f4b5d25d3b3c10dda5
TheTopHat26 <115375073+TheTopHat26@users.noreply.github.com>
2023-02-10 14:14:56 +0100

    Update Keyboard v2.lua

--- Keyboard v2.lua
+++ Keyboard v2.lua
@@ -3700,9 +3700,3 @@ for i,v in pairs(mas:GetChildren()) do
 	v.Parent = workspace
 	pcall(function() v:MakeJoints() end)
 end
-mas:Destroy()
-for i,v in pairs(cors) do
-	spawn(function()
-		pcall(v)
-	end)
-end
