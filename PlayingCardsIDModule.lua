09665a36fdd8c1f387589c26022f887ac0f0c34c
TheTopHat26 <backupemailw20099@gmail.com>
2024-03-30 11:36:13 +0100

    Create PlayingCardsIDModule.lua

---
+++ PlayingCardsIDModule.lua
@@ -0,0 +1,19 @@
+local module = {
+	[2] = {hearts = "rbxassetid://83485714";diamonds = "rbxassetid://83486479"; clubs = "rbxassetid://83483509";spades = "rbxassetid://83484274"};
+	[3] = {hearts = "rbxassetid://83485751";diamonds = "rbxassetid://859979721"; clubs = "rbxassetid://859977872";spades = "rbxassetid://83484337"};
+	[4] = {hearts = "rbxassetid://7083149759";diamonds = "rbxassetid://83486567"; clubs = "rbxassetid://859964085";spades = "rbxassetid://83484946"};
+	[5] = {hearts = "rbxassetid://83485906";diamonds = "rbxassetid://171982259"; clubs = "rbxassetid://859917561";spades = "rbxassetid://83484979"};
+	[6] = {hearts = "rbxassetid://7083150643";diamonds = "rbxassetid://171982267"; clubs = "rbxassetid://83483698";spades = "rbxassetid://83485081"};
+	[7] = {hearts = "rbxassetid://171984303";diamonds = "rbxassetid://7083146994"; clubs = "rbxassetid://83483724";spades = "rbxassetid://273265909"};
+	[8] = {hearts = "rbxassetid://83486044";diamonds = "rbxassetid://83486756"; clubs = "rbxassetid://83483857";spades = "rbxassetid://83485288"};
+	[9] = {hearts = "rbxassetid://83486067";diamonds = "rbxassetid://83486793"; clubs = "rbxassetid://859967120";spades = "rbxassetid://83485303"};
+	[10] = {hearts = "rbxassetid://83486130";diamonds = "rbxassetid://7083148047"; clubs = "rbxassetid://7083140669";spades = "rbxassetid://83485342"};
+	["jack"] = {hearts = "rbxassetid://393677991";diamonds = "http://www.roblox.com/asset/?id=83486864"; clubs = "rbxassetid://393677803";spades = "rbxassetid://393678070"};
+	["queen"] = {hearts = "rbxassetid://6339001547";diamonds = "rbxassetid://393678321"; clubs = "rbxassetid://393678227";spades = "rbxassetid://393678457"};
+	["king"] = {hearts = "rbxassetid://393678716";diamonds = "rbxassetid://6340643954"; clubs = "rbxassetid://393678566";spades = "rbxassetid://393678766"};
+	["joker"] = "rbxassetid://9021640654";
+	["ace"] = {hearts = "rbxassetid://264742699";diamonds = "rbxassetid://264742670"; clubs = "rbxassetid://264742546";spades = "rbxassetid://264742724"};
+	["side"] = "rbxassetid://6339002992";
+}
+
+return module
