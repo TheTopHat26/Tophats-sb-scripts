193f9e3c61fa9e4261f3cc1a3f6367ea0958570c
TheTopHat26 <backupemailw20099@gmail.com>
2024-04-02 20:31:40 +0200

    Create Equip.lua

---
+++ Animations/Errorcrowbar/Equip.lua
@@ -0,0 +1,4 @@
+-- // converted using mech's animoduler
+local cfr = CFrame.new
+
+return {{tm=0;["Right Arm"]={tm=.267;cf=cfr(0,0,0,.899,.438,0,-.438,.899,0,0,0,1);es="Cubic";ed="In";};};{tm=.267;["Right Arm"]={tm=.2329;cf=cfr(-.25,0,0,-.342,-.94,0,.94,-.342,0,0,0,1);es="Cubic";ed="In";};};{tm=.5;["Right Arm"]={tm=0;cf=cfr(-.4,-.3,0,0,-1,0,1,0,0,0,0,1);es="Cubic";ed="In";};};}
