75bed8b72dca3d18e354ac90af91e9d0b3f510eb
TheTopHat26 <backupemailw20099@gmail.com>
2024-04-02 20:31:25 +0200

    Create attack1.lua

---
+++ Animations/Errorcrowbar/attack1.lua
@@ -0,0 +1,4 @@
+-- // converted using mech's animoduler
+local cfr = CFrame.new
+
+return {{tm=0;["Right Arm"]={tm=.067;cf=cfr(-.4,-.3,0,0,-1,0,1,0,0,0,0,1);es="Cubic";ed="In";};};{tm=.067;["Right Arm"]={tm=.066;cf=cfr(-.2,-.3,0,-.309,-.951,0,.951,-.309,0,0,0,1);es="Linear";ed="In";};};{tm=.133;["Right Arm"]={tm=.067;cf=cfr(.05,0,0,-.139,-.99,0,.99,-.139,0,0,0,1);es="Linear";ed="In";};};{tm=.2;["Right Arm"]={tm=0;cf=cfr(-.25,0,0,.174,-.985,0,.985,.174,0,0,0,1);es="Linear";ed="In";};};}
